09e7e08527310ec454d797fedf90a43e9f3871c3
Chris <chris@tracktownsoftware.com>
2021-04-21 08:43:00 -0700

    test

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# MultipleSolutionsTest_WPF
+### MultipleSolutionsTest_Windows 
+This repository contains a subset of folders and files targeting Windows applications from repository MultipleSolutionsTest. 
+
+For more information go to [MultipleSolutionsTest ReadMe.md](https://github.com/tracktownsoftware/MultipleSolutionsTest/blob/master/ReadMe.md).
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var helloWorld = new SharedClassLibrary1.HelloWorld();
+            label1.Text = helloWorld.SayHelloWorld;
+        }
+    }
+}
